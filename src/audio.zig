86811628d70612c0181e38c7ace3e70d94fdd4fa
spooky-th-ghost <colin@wildheartinteractive.com>
2025-02-22 20:23:01 -0500

    added goblet sound

--- src/audio.zig
+++ src/audio.zig
@@ -5,6 +5,7 @@ pub const SoundBank = struct {
     footstep: rl.Sound,
     rock: rl.Sound,
     key: rl.Sound,
+    goblet: rl.Sound,
 
     const Self = @This();
 
@@ -13,6 +14,7 @@ pub const SoundBank = struct {
             .footstep = try rl.loadSound("assets/sounds/footstep.wav"),
             .rock = try rl.loadSound("assets/sounds/rock.wav"),
             .key = try rl.loadSound("assets/sounds/key.wav"),
+            .goblet = try rl.loadSound("assets/sounds/goblet.wav"),
         };
     }
 };
